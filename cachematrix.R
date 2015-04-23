47506adfa840ddbb93cc350940c5af0fc5b092c2
queenie-chan <queenie.chan@gmail.com>
2015-04-22 22:04:25 -0400

    added code to cachematrix.R file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates special object of functions for a matrix.
+    ## matrix can be set (write), get (read), 
+    ## setinv (write inverse), getinv (read inverse)
+## cacheSolve computes the inverse of a matrix via cache, if inverse is cached
+    ## otherwise, solves inverse and sets matrix's inverse via makeCacheMatrix
 
-## Write a short comment describing this function
 
+## makeCacheMatrix creates a special "matrix" object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y         # assign the input, y, as x in parent fxn (makeCacheMatrix)
+        inv <<- NULL    # resets inverse of x
+    }
+    get <- function() x # returns x
+    setinv <- function(inverse) inv <<- inverse
+                        # sets inv in parent fxn as input, inverse
+    getinv <- function() inv
+                        # returns inv
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve computes inverse of "matrix" from makeCacheMatrix
+    # If inverse already calculated, cacheSolve retrieves inverse from cache
+    # Else, computes inverse and sets matrix's inverse
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Return a matrix that is the inverse of 'x'
+    
+    inv <- x$getinv()           # read inverse from x
+    if (!is.null(inv)) {        # if inv exists, grab cached value
+        message("retrieving cached inverse")
+        return(inv)             # return cached inv
+    }         
+    inv <- solve(x$get(), ...)  # else, calc inverse
+    x$setinv(inv)               # set inverse in inputted matrix
+    inv                         # return inv
 }
